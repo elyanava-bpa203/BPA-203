27a3256e4ebaca105d687c534dcc1bbf5b4193f1
elyanava-bpa203 <elyanava-bpa203@code.edu.az>
2025-10-09 13:17:47 +0400

    added loops

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿////Task 1-Simple calculator
+
+////double a = 10;
+////double b = 5;
+////char op = '+';
+
+////if (op == '+' )
+////    Console.WriteLine( "Netice:" + (a+b));
+////else if (op == '-')
+////    Console.WriteLine( "Netice:" + (a-b));
+////else if (op == '*' )
+////    Console.WriteLine( "Netice:" + (a*b));
+////else if (op == '/' )
+////    Console.WriteLine( "Netice:" + (a/b));
+////else
+////    Console.WriteLine("Duzgun emeliyyat secin");
+
+
+
+
+////Task 2-Temperature status
+
+////int temp = 32;
+////if (temp <= 0)
+////{
+////    Console.WriteLine("Freezing");
+////}
+////else if (temp >= 0 && temp <= 15) 
+////{
+////    Console.WriteLine("Cold");
+////}
+
+////else if (temp >=16 && temp <= 25) 
+////{
+////    Console.WriteLine("Normal");
+////}
+////else if (temp >= 26 && temp <= 35) 
+////{
+////    Console.WriteLine("Warm");
+////}
+////else
+////{
+////    Console.WriteLine("Hot");
+
+
+
+
+
+////Task 3-Salary bonus calculator
+
+//double maas = 800;
+//double tecrube = 4;
+//if (tecrube >= 0 && tecrube <= 2)
+//{
+//    Console.WriteLine("Bonus = maas *0.05");
+//}
+//else if (tecrube >= 3 && tecrube <= 5)
+//{
+//    Console.WriteLine("Bonus = maas *0.1");
+//}
+//else if (tecrube >= 6 && tecrube <= 10)
+//{
+//    Console.WriteLine("Bonus = maas *0.15");
+//}
+//else if (tecrube > 10)
+//{
+//    Console.WriteLine("Bonus = maas *0.2");
+//}
+//else
+//{
+//    Console.WriteLine("Bonus verilmir");
+//}
