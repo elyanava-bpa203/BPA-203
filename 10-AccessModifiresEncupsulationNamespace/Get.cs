d011db539d7dba1569a276e6fd8e168c738b5af1
elyanava-bpa203 <elyanava-bpa203@code.edu.az>
2025-10-22 11:18:33 +0400

    accessmodifiers added

---
+++ 10-AccessModifiresEncupsulationNamespace/Get.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_AccessModifiresEncupsulationNamespace
+{
+    internal class Get
+    {
+
+        private int age;
+        public int  Age
+        {
+            get 
+            {
+                return age;
+            
+            }
+            set 
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("You are not an adult yet.");
+                    return;
+                
+                }
+                age = value;
+            
+            }
+        
+        }
+    }
+}
---
+++ 10-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _10_AccessModifiresEncupsulationNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Get get = new Get();
+            get.Age = 20;
+            Console.WriteLine("Age: " + get.Age);   
+        }
+    }
+}
